5f33e35999ff8a1816ce23e2308e2cb8883847a7
sandeep <sandeepreddy281@gmail.com>
2017-03-20 15:38:38 +0530

    updated

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -2,8 +2,9 @@ angular.module('app.routes',['ngRoute'])
 		.config(function($routeProvider,$locationProvider){
 			$routeProvider
 				.when('/', {
-					templateUrl : 'app/views/pages/posts.html',
-					controller   : 'getAllPostsController'
+					templateUrl : 'app/views/pages/login.html',
+					controller : 'mainController',
+					controllerAs: 'login'
 				})
 
 				.when('/login', {
