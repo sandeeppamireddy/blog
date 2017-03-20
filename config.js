9b891e9143b9e02182bcdd8618e3cd683178028e
sandeep <sandeepreddy281@gmail.com>
2017-03-20 16:51:51 +0530

    db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost:27017/node-test',
+	'database': 'mongodb://sandeep:sandeep@ds137100.mlab.com:37100/node-test',
 	'secret':'trytohack'
 };
